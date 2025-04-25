657024ca37c6c78f030dc7fd28dda25c7719134d
0xScorpio <140411254+0xScorpio@users.noreply.github.com>
2025-04-25 06:47:49 +0300

    Update directory_buster.sh

--- 00-Methodology/02-Enumeration/Port-80,443,8080,8000/directory_buster.sh
+++ 00-Methodology/02-Enumeration/Port-80,443,8080,8000/directory_buster.sh
@@ -8,7 +8,14 @@ fi
 
 # Commands for directory brute-forcing
 FEROX_CMD="feroxbuster -u http://"
-FEROX_ARGS="-w /usr/share/wordlists/seclists/Discovery/Web-Content/directory-list-2.3-medium.txt --filter-status 400,401,403,404,503"
+FEROX_ARGS="-w /usr/share/wordlists/seclists/Discovery/Web-Content/directory-list-lowercase-2.3-big.txt --filter-status 400,402,403,404,501,502,503,504,505"
+
+GOBUSTER_CMD="gobuster dir -u http://"
+GOBUSTER_ARGS="-w /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt -t 5 -b 200,301,302"
+
+FFUF_CMD="ffuf -u http://"
+FFUF_FUZZ="/FUZZ"
+FFUF_ARGS="-w /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt -e .php,.html,.asp,.aspx,.bak,.old,.orig,.tmp,.txt,.log,.env,.xml,.json,.yml,.conf,.ini,.zip,.tar,.gz,.rar,.md,.jsp,.sqp,.swo -r -t 100 -mc 200,301,302 -c"
 
 DIRSEARCH_CMD="dirsearch -u http://"
 DIRSEARCH_ARGS="-w /usr/share/wordlists/seclists/Discovery/Web-Content/common.txt -t 50 --exclude-status 400,401,403,404,503"
@@ -22,7 +29,17 @@ run_in_tab() {
   # Run Feroxbuster in the first split (default terminal)
   xdotool type "$FEROX_CMD$target $FEROX_ARGS" && xdotool key Return
   sleep 2
+  
+  # Split terminal (RIGHT) and run GoBuster
+  xdotool key ctrl+shift+r
+  xdotool type "$GOBUSTER_CMD$target $GOBUSTER_ARGS" && xdotool key Return
+  sleep 2
 
+  # Split terminal (DOWN) and run FFUF
+  xdotool key ctrl+shift+d
+  xdotool type "$FFUF_CMD$target$FFUF_FUZZ $FFUF_ARGS" && xdotool key Return
+  sleep 2
+  
   # Split terminal (DOWN) and run Dirsearch
   xdotool key ctrl+shift+d
   sleep 2
