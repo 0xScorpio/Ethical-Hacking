6912b1d84eefacd475f55460b4691900461a966d
0xScorpio <140411254+0xScorpio@users.noreply.github.com>
2025-04-29 01:03:55 +0300

    Update directory_buster.sh

--- 00-Methodology/02-Enumeration/Port-80,443,8080,8000/directory_buster.sh
+++ 00-Methodology/02-Enumeration/Port-80,443,8080,8000/directory_buster.sh
@@ -13,7 +13,6 @@ FEROX_CMD="feroxbuster -u http://$TARGET -w /usr/share/wordlists/seclists/Discov
 GOBUSTER_CMD="gobuster dir -u http://$TARGET -w /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt -t 5 -b 404,501,502,503,504,505"
 FFUF_CMD="ffuf -u http://$TARGET/FUZZ -w /usr/share/wordlists/seclists/Discovery/Web-Content/raft-large-directories.txt -e .php,.html,.asp,.aspx,.bak,.old,.orig,.tmp,.txt,.log,.env,.xml,.json,.yml,.conf,.ini,.zip,.tar,.gz,.rar,.md,.jsp,.sqp,.swo -r -t 100 -mc 200,301,302 -c"
 DIRSEARCH_CMD="dirsearch -u http://$TARGET -w /usr/share/wordlists/seclists/Discovery/Web-Content/common.txt -t 50 --exclude-status 400,401,403,404,503"
-PAGECHECK_CMD="pgcheck http://$TARGET"
 
 # Function to open new tab, split, and run commands
 run_in_tab() {
@@ -35,11 +34,6 @@ run_in_tab() {
   xdotool key ctrl+shift+d
   sleep 2
   xdotool type "$DIRSEARCH_CMD" && xdotool key Return
-  
-  # Split terminal (DOWN) and run pgcheck
-  xdotool key ctrl+shift+d
-  sleep 2
-  xdotool type "$PAGECHECK_CMD" && xdotool key Return
 }
 
 # Start the workflow
